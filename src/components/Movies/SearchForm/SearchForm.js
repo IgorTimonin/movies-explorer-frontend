2ffb55bc2aadee1446cae4f822c317e807800de9
Igor Timonin <mut4nt@yandex.ru>
2022-10-03 14:56:57 +0300

    fix search form focus&requared

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -4,11 +4,11 @@ export default function SearchForm(props) {
   return (
     <section className="searchForm">
       <div className="searchBar">
-        <div className="searchBar__finder">
+        <form className="searchBar__finder">
           <div className="searchBar__icon searchBar__icon_hide"></div>
-          <input className="searchBar__input" placeholder="Фильм"></input>
-          <div className="searchBar__icon searchBar__submit app__btn-opacity"></div>
-        </div>
+          <input className="searchBar__input" placeholder="Фильм" required></input>
+          <button type='submit' className="searchBar__icon searchBar__submit app__btn-opacity"></button>
+        </form>
 
         <div className="searchBar__controls">
           <div className="searchBar__separator searchBar__icon_hide"></div>
